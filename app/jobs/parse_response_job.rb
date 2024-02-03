b28dce1f78a6f6241ab0b7e3b00bdeaae1de3623
Brad Feehan <git@bradfeehan.com>
2024-02-03 14:39:58 +1100

    Create ParseResponseJob

---
+++ app/jobs/parse_response_job.rb
@@ -0,0 +1,21 @@
+# typed: strict
+# frozen_string_literal: true
+
+# Parses a specific response from an API
+class ParseResponseJob < ApplicationJob
+  extend T::Generic
+  extend T::Sig
+
+  queue_as :default
+
+  sig { params(response: Response[T.untyped, T.untyped]).void }
+  def perform(response:)
+    Rails.logger.info { "Fetching response: #{response.inspect}" }
+    Rails.logger.info { "Respondable: #{response.try(:respondable)&.inspect}" }
+
+    entities = response.parse!
+    Rails.logger.info { "Got entities: #{entities.first.inspect}" }
+
+    entities.map(&:save!)
+  end
+end
--- app/models/responses/overpass_response.rb
+++ app/models/responses/overpass_response.rb
@@ -1,14 +1,95 @@
 # typed: strict
 # frozen_string_literal: true
 
+require 'delegate'
+
 module Responses
   # Represents a response from querying the Overpass API
   class OverpassResponse < Response
+    extend T::Generic
     extend T::Sig
 
-    sig { override.returns(T::Array[T.untyped]) }
+    Element = type_member { { fixed: OverpassElement } }
+    Model = type_member { { fixed: Facility } }
+
+    sig { override.returns(T::Enumerable[Model]) }
     def parse!
-      raise NotImplementedError, 'TODO: Implement osm2geojson, but instead of GeoJSON files create PostGIS entities'
+      elements.map do |element|
+        Rails.logger.debug { "Parsing element: #{element.id} '#{element.name}'" }
+        facility_for(element)
+      end
+    end
+
+    private
+
+    sig { params(element: Element).returns(Model) }
+    def facility_for(element)
+      # TODO: associate created facilities with this response, i.e. facilities.build(...)
+      # a join table with response_id, parseable_type, parseable_id
+      # extract some "Parseable" concern -- a model that can be parsed from a Response model
+      # associate each Parseable with the Response it was parsed from
+      Facility.new(external_id: element.id, name: element.name).tap do |facility|
+        Rails.logger.debug { "Creating geocode: lat=#{element.latitude} lon=#{element.longitude}" }
+        facility.geocodes.build(latitude: element.latitude, longitude: element.longitude)
+      end
+    end
+
+    sig { returns(T::Enumerable[Element]) }
+    def elements
+      nodes.map { |node| OverpassElement.new({}.replace(node)) }
+    end
+
+    sig { returns(T::Enumerator[Saxerator::Builder::HashElement]) }
+    def nodes
+      parser.for_tag(:node).lazy
+    end
+
+    sig { returns(Saxerator::FullDocument) }
+    def parser
+      @parser ||= T.let(
+        Saxerator.parser(body) do |config|
+          config.adapter = :ox
+          config.output_type = :hash
+          config.put_attributes_in_hash!
+          config.symbolize_keys!
+        end,
+        T.nilable(Saxerator::FullDocument),
+      )
+    end
+
+    # Represents a single node in the Overpass API response
+    class OverpassElement < SimpleDelegator
+      include Kernel
+      extend T::Sig
+
+      delegate :inspect, to: :__getobj__
+      alias to_s inspect
+
+      sig { params(node: T::Hash[Symbol, T.untyped]).void }
+      def initialize(node)
+        @node = node
+        super(node)
+      end
+
+      sig { returns(String) }
+      def id
+        @node.fetch(:id)
+      end
+
+      sig { returns(String) }
+      def name
+        @node.fetch(:tag).find { _1[:k] == 'name' }&.fetch(:v)
+      end
+
+      sig { returns(Numeric) }
+      def latitude
+        Float(@node.fetch(:lat))
+      end
+
+      sig { returns(Numeric) }
+      def longitude
+        Float(@node.fetch(:lon))
+      end
     end
   end
 end
